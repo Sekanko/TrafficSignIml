a298c23ccd587f3cb53d9eed18aceb4ac20a2bca
dmnrcznskk <dmnrcznsk@gmail.com>
2026-02-04 00:04:20 +0100

    Dodalem augmentacje dla cnn oraz oversapmpling ogolnie

--- src/models/cnn_model.py
+++ src/models/cnn_model.py
@@ -1,24 +1,37 @@
 from sklearn.metrics import classification_report
 import numpy as np
+import tensorflow as tf
 from tensorflow.keras import layers, models
 
 from src.data.preprocess_img import preprocess_image
 from src.models.run_model import run_model
 
-
 def build_cnn(input_shape, num_classes):
-    model = models.Sequential(
-        [
-            layers.Conv2D(32, (3, 3), activation="relu", input_shape=input_shape),
-            layers.MaxPooling2D((2, 2)),
-            layers.Conv2D(64, (3, 3), activation="relu"),
-            layers.MaxPooling2D((2, 2)),
-            layers.Conv2D(128, (3, 3), activation="relu"),
-            layers.Flatten(),
-            layers.Dense(128, activation="relu"),
-            layers.Dense(num_classes, activation="softmax"),
-        ]
-    )
+    data_augmentation = models.Sequential([
+        layers.Rescaling(255.0),
+        layers.RandomRotation(0.05), 
+        layers.RandomZoom(0.1),
+        layers.RandomTranslation(0.05, 0.05), 
+        layers.Rescaling(1./255)
+    ], name="data_augmentation")
+
+    model = models.Sequential([
+        layers.Input(shape=input_shape),
+        
+        data_augmentation,
+
+        layers.Conv2D(32, (3, 3), activation="relu"),
+        layers.MaxPooling2D((2, 2)),
+        
+        layers.Conv2D(64, (3, 3), activation="relu"),
+        layers.MaxPooling2D((2, 2)),
+        
+        layers.Conv2D(128, (3, 3), activation="relu"),
+        layers.Flatten(),
+        
+        layers.Dense(128, activation="relu"),
+        layers.Dense(num_classes, activation="softmax"),
+    ])
 
     model.compile(
         optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
@@ -30,12 +43,13 @@ def train_cnn(model, train_df, val_df, epochs=10, batch_size=32):
     print("Przygotowanie danych treningowych i walidacyjnych dla CNN...")
     
     X_train = preprocess_image(train_df["Path"])
-    y_train = train_df["ClassId"].values
+    y_train = train_df["ClassId"].values.astype('int')
 
     X_val = preprocess_image(val_df["Path"])
-    y_val = val_df["ClassId"].values
+    y_val = val_df["ClassId"].values.astype('int')
 
-    print(f"Start treningu CNN...")
+    print(f"Start treningu CNN (z wbudowaną augmentacją)...")
+    
     history = model.fit(
         X_train,
         y_train,
@@ -57,7 +71,7 @@ def load_cnn(path="cnn_model.keras"):
 
 def evaluate_cnn(model, test_df):
     X_test = preprocess_image(test_df["Path"])
-    y_test = test_df["ClassId"].values
+    y_test = test_df["ClassId"].values.astype('int')
     
     loss, acc = model.evaluate(X_test, y_test, verbose=0)
     print(f"CNN Test Accuracy: {acc:.4f}")
